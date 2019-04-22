3856053903ff7a7ce9b43418b3b52e04ebe3afef
AlphaIn <mymail_123@yeah.net>
2019-04-22 19:59:58 +0800

    Create Consumer.java
    消费者

---
+++ Producer_Consumer/Consumer.java
@@ -0,0 +1,30 @@
+package java.Producer_Consumer;
+
+import java.util.LinkedList;
+
+public class Consumer extends Thread{
+	private LinkedList<Character> list;
+
+	public Consumer(LinkedList<Character> list) {
+		this.list = list;
+	}
+
+	@Override
+	public void run() {
+		while(true) {
+			synchronized (list) {
+				if(list.size() == 0) {
+					//消费者线程在集合对象上等待
+					try {
+						list.wait();
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+					}
+				}
+			Character c = list.removeFirst();
+			System.out.println("移除>>>>>>>>>>>>>"+c);
+			}
+		}
+	}
+	
+}
