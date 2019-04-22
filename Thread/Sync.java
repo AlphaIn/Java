6331d44937e8aa9bc122959fbc2622eac9572e41
AlphaIn <mymail_123@yeah.net>
2019-04-22 19:01:17 +0800

    Create Sync.java
    线程的同步锁

---
+++ Thread/Sync.java
@@ -0,0 +1,40 @@
+package java.Thread;
+
+import java.util.Arrays;
+
+public class Sync {
+
+	static char[] a = {'*','*','*','*','*'};
+	static char c = '-';
+	public static void main(String[] args) {
+		new Thread( ) {
+
+			@Override
+			public void run() {
+				while (true) {
+					synchronized(a) {
+						for (int i = 0; i<a.length; i++) {
+							a[i] = c;
+						}
+					}
+					c = (c=='-'?'*':'-');
+				}
+			}
+			
+		}.start();
+		
+        new Thread() {
+
+			@Override
+			public void run() {
+				while(true) {
+					synchronized(a) {
+					System.out.println(Arrays.toString(a));
+					}
+				}
+			}
+        	
+        }.start();
+	}
+
+}
