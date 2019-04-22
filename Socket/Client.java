5e375451e447da9ca7b62d0378962d54730b54dc
AlphaIn <mymail_123@yeah.net>
2019-04-22 23:53:46 +0800

    Create Client.java
    java socket client

---
+++ Socket/Client.java
@@ -0,0 +1,28 @@
+package java.Socket
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class Client {
+	
+	public static void main (String[] args) throws Exception {
+		
+		//向服务器发起连接
+		Socket s = new Socket("127.0.0.1",8000);
+		
+		//从socket插头对象，取出双向的流
+		InputStream in = s.getInputStream();
+		OutputStream out = s.getOutputStream();
+		
+		//通信
+		out.write("hello".getBytes());
+		for (int i = 0; i < 5; i++) {
+			char c = (char)in.read();
+			System.out.print(c);
+		}
+		s.close();
+	}
+}
