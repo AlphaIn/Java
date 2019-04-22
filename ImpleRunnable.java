e2a9ddd25712b3d0ff6bb826bc48128dd6880e85
AlphaIn <mymail_123@yeah.net>
2019-04-22 19:42:45 +0800

    Create ImpleRunnable.java
    通过实现runnable方法实现多线程

---
+++ ImpleRunnable.java
@@ -0,0 +1,43 @@
+package java.ImpleRunnable;
+
+public class ImpleRunnable {
+
+	public static void main(String[] args) {
+		R1 r1 = new R1();
+		//Thread t1 = new Thread(r1);
+		//t1.start();
+		new Thread(r1) {
+			
+		}.start();
+		//main线程
+		R1 r2 = new R1();
+		while(true) {
+			int i = r2.get();
+			if(i%2 == 1) {
+				System.out.println(i);
+				System.exit(0);
+			}
+		}
+
+	}
+	
+	static class R1 implements Runnable {
+		static int i = 0;
+		static synchronized void add() {
+			i++;
+			i++;
+		}
+		static synchronized int get() {
+			return i;
+		}
+		@Override
+		public void run() {
+			while(true) {
+				add();
+			}
+			
+		}
+		
+	}
+
+}
