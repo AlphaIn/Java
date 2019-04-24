9e0e987edaebe159c765b26d53b3bdae9933cec0
AlphaIn <mymail_123@yeah.net>
2019-04-24 21:33:10 +0800

    Update ArrayApplicationDemo.java

--- Array/ArrayApplicationDemo.java
+++ Array/ArrayApplicationDemo.java
@@ -4,6 +4,18 @@ public class ArrayApplicationDemo {
 	public static void main(String[] args) {
 		int[] arr = {2,8,9,2,7,3,0,8,3,1,4};
 		
+		//数组的反转
+		for (int i = 0, j = arr.length -1; i < j; i++,j--) {
+			
+			int temp = arr[i];
+			arr[i] = arr[j];
+			arr[j] = temp;
+			
+		}
+		
+		String str0 = Arrays.toString(arr);
+		System.out.println(str0);
+		
 		//获取数组的长度
 		int len = arr.length;
 		System.out.println(len);
@@ -49,5 +61,31 @@ public class ArrayApplicationDemo {
 		}
 		String str2 = Arrays.toString(arr1);
 		System.out.println(str2);
+		
+		//二分查找
+		int[] arr2 = {14,26,37,58,64,68,82,90};
+		int num = 58;
+		
+		int min = 0;
+		int max = arr.length - 1;
+		int mid = (min + max) / 2;
+		
+		while(arr2[mid] != num) {
+			
+			if(num > arr2[mid]) 
+				min = mid + 1;
+			else 
+				max = mid - 1;
+			
+			if (min > max) {
+				mid = -1;
+				break;
+			}
+			
+			mid = (min + max) / 2;
+			
+		}
+		
+		System.out.println(mid);
 	}
 }
